0687d657a59ef76798adf315410c61e60dfef19b
Gxming <1641092084@qq.com>
2018-08-03 15:24:17 +0800

    typescrit 全局声明

---
+++ src/a.ts
@@ -0,0 +1,6 @@
+import { libB1 } from 'ts_test/src/lib2'
+
+export function b() {
+    console.log(libB1)
+}
+export default libB1
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+/// <reference path="./sfc.d.ts" />
 import Vue from 'vue'
 import "es6-promise"
 import App from './app.vue'
 // import iView from 'iview'
-import 'iview/dist/styles/iview.css'
+// import 'iview/dist/styles/iview.css'
+import { test, msg } from 'ts_test'
+import { libA } from 'ts_test/src/lib1'
+import { libB1, libB2 } from 'ts_test/src/lib2'
 
 new Vue({
     el: "#app",
@@ -11,5 +15,23 @@ new Vue({
         return h('App')
     }
 })
+
+
+//1
+test()
+msg()
+
+//2
+let testlibA = new libA()
+console.log(testlibA.get())
+testlibA.set(444)
+console.log(testlibA.get())
+
+//3
+console.log(libB1)
+console.log(libB2)
+
+
+// https://blog.csdn.net/letterTiger/article/details/80596369
 
 
